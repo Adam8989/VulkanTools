06de9f6a812b50442129e0be32eea88586ceedfc
Mike Weiblen <mikew@lunarg.com>
2017-09-05 21:24:12 -0600

    devsim_layer_test : rearrange

--- tests/devsim_layer_test.sh
+++ tests/devsim_layer_test.sh
@@ -23,19 +23,20 @@ export LD_LIBRARY_PATH=${PWD}/../loader:${LD_LIBRARY_PATH}
 export VK_LAYER_PATH=${PWD}/../layersvt
 export VK_INSTANCE_LAYERS="VK_LAYER_LUNARG_device_simulation"
 
-# Test input datafile, and filename of output.
-FILENAME_01_IN="devsim_test1.json"
-FILENAME_01_GOLD="devsim_test1_gold.json"
-FILENAME_01_OUT="device_simulation_layer_test_1.json"
-
-export VK_DEVSIM_FILENAME="${PWD}/${FILENAME_01_IN}"
-
 #export VK_DEVSIM_DEBUG_ENABLE="1"
 #export VK_DEVSIM_EXIT_ON_ERROR="1"
 #export VK_LOADER_DEBUG="all"
 
 VKJSON_INFO="${PWD}/../libs/vkjson/vkjson_info"
 
+#############################################################################
+# Test #1 input datafile, and filename of output.
+
+FILENAME_01_IN="devsim_test1.json"
+FILENAME_01_GOLD="devsim_test1_gold.json"
+FILENAME_01_OUT="device_simulation_layer_test_1.json"
+
+export VK_DEVSIM_FILENAME="${PWD}/${FILENAME_01_IN}"
 ${VKJSON_INFO} > /dev/null
 
 # compare vkjson output against gold
@@ -43,6 +44,8 @@ NUM_LINES=$(cut -f1 -d' ' <(wc -l ${FILENAME_01_GOLD}))
 diff ${FILENAME_01_GOLD} <(head -n ${NUM_LINES} ${FILENAME_01_OUT}) >/dev/null
 RES=$?
 rm ${FILENAME_01_OUT}
+
+#############################################################################
 
 if [ "$RES" -eq 0 ] ; then
    printf "$GREEN[  PASSED  ]$NC ${PGM}\n"

0e7245f2747ada4290af9e1f420fc0a0af240f95
Mike Weiblen <mikew@lunarg.com>
2017-09-08 12:08:35 -0600

    devsim: list of requested checks

--- layersvt/device_simulation.cpp
+++ layersvt/device_simulation.cpp
@@ -456,6 +456,26 @@ void JsonLoader::ApplyOverrides(const Json::Value &value, VkPhysicalDeviceLimits
         return;
     }
 
+/*
+
+uint32_t              maxBoundDescriptorSets;
+    uint32_t              maxPerStageDescriptorSamplers;
+    uint32_t              maxPerStageDescriptorUniformBuffers;
+    uint32_t              maxPerStageDescriptorStorageBuffers;
+    uint32_t              maxPerStageDescriptorSampledImages;
+    uint32_t              maxPerStageDescriptorStorageImages;
+    uint32_t              maxPerStageDescriptorInputAttachments;
+    uint32_t              maxPerStageResources;
+    uint32_t              maxDescriptorSetSamplers;
+    uint32_t              maxDescriptorSetUniformBuffers;
+    uint32_t              maxDescriptorSetUniformBuffersDynamic;
+    uint32_t              maxDescriptorSetStorageBuffers;
+    uint32_t              maxDescriptorSetStorageBuffersDynamic;
+    uint32_t              maxDescriptorSetSampledImages;
+    uint32_t              maxDescriptorSetStorageImages;
+    uint32_t              maxDescriptorSetInputAttachments;
+*/
+
     GET_VALUE(maxImageDimension1D);
     GET_VALUE(maxImageDimension2D);
     GET_VALUE(maxImageDimension3D);
